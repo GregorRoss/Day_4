337aeb38c5d25e524344336b619d1399f57cac86
Gregor <gregor.home@btinternet.com>
2023-03-23 15:53:26 +0000

    First save

---
+++ runner.py
@@ -0,0 +1,14 @@
+#import calculator as safe_calc 
+#from calculator import add, subtrct
+#from calculator import *
+#from maths.calculator import add, subtract
+import maths.calculator as calc
+
+
+
+result = calc.add(1,2)
+result2 = calc.subtract(12,1)
+print(result)
+print(result2)
+
+
